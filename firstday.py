4e74522b6dd4aada68f5f4473828c9a87ec053b2
David Prates <davidjr.com@hotmail.com>
2023-06-08 21:47:44 +0100

    End of the first day

---
+++ firstday.py
@@ -0,0 +1,55 @@
+print ("Day 1 - Python Print Function")
+print ("The function is declared like this:")
+print ("print('what to print')")
+
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("""String Concatenation is done with the "+" sign.""")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#Write your code below this line 👇
+
+name_len=len(input("What is your name?"))
+print(name)
+
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c=a
+d=b
+a=d
+b=c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+#1. Create a greeting for your program.
+
+print("Welcome to David's program!!!")
+
+#2. Ask the user for the city that they grew up in.
+name=input("What is your name?")
+
+#3. Ask the user for the name of a pet.
+pet_name=input("What is the name of your pet?")
+
+#4. Combine the name of their city and pet and show them their band name.
+combinedname = name + pet_name
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
